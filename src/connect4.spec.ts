d24b07c7d8542eef31ebe622150ef7642576e703
fzaninotto <fzaninotto@gmail.com>
2025-01-16 18:24:53 +0100

    move tests

--- src/connect4.spec.ts
+++ src/connect4.spec.ts
@@ -4,6 +4,8 @@ import {
   countNbTokens,
   initGameState,
   playToken,
+  getWinner,
+  isFull,
 } from "./connect4";
 import { GameState, PlayerNum } from "./types/gameState";
 import {
@@ -195,5 +197,178 @@ describe("Entry point 'initGameState'", () => {
         `Cannot add token into column ${colNum} which is full.`,
       );
     });
+  });
+});
+
+describe("getWinner", () => {
+  const emptyBoard = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+  ];
+  const ongoingBoard = [
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0],
+    [0, 2, 1, 0, 2, 0, 0],
+    [0, 2, 1, 1, 2, 0, 2],
+    [0, 1, 1, 2, 1, 1, 1],
+    [0, 2, 2, 1, 1, 2, 2],
+  ];
+  const drawBoard = [
+    [1, 1, 2, 2, 1, 1, 2],
+    [2, 2, 1, 1, 2, 2, 1],
+    [1, 1, 2, 2, 1, 1, 2],
+    [2, 2, 1, 1, 2, 2, 1],
+    [1, 1, 2, 2, 1, 1, 2],
+    [2, 2, 1, 1, 2, 2, 1],
+  ];
+  it.each([
+    ["empty", emptyBoard, 0],
+    ["ongoing", ongoingBoard, 0],
+    ["draw", drawBoard, 0],
+  ])("should return 0 if the board is %s", (_msg, board, expectedWinner) => {
+    expect(getWinner(board)).toBe(expectedWinner);
+  });
+  const horizontalWin1 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 1, 0, 2, 0, 0],
+    [1, 1, 1, 1, 2, 0, 2],
+    [2, 1, 1, 2, 1, 1, 1],
+    [2, 2, 2, 1, 1, 2, 2],
+  ];
+  const horizontalWin2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 2, 0, 1, 0, 0],
+    [2, 2, 2, 2, 1, 0, 1],
+    [1, 2, 2, 1, 2, 2, 2],
+    [1, 1, 1, 2, 2, 1, 1],
+  ];
+  it.each([
+    ["1", horizontalWin1, 1],
+    ["2", horizontalWin2, 2],
+  ])(
+    "should return %s if they have a horizontal four",
+    (_msg, board, expectedWinner) => {
+      expect(getWinner(board)).toBe(expectedWinner);
+    },
+  );
+  const verticalWin1 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [2, 0, 0, 0, 1, 0, 0],
+    [2, 0, 0, 0, 2, 0, 0],
+  ];
+  const verticalWin2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0],
+    [1, 0, 0, 0, 2, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0],
+  ];
+  it.each([
+    ["1", verticalWin1, 1],
+    ["2", verticalWin2, 2],
+  ])(
+    "should return %s if they have a vertical four",
+    (_msg, board, expectedWinner) => {
+      expect(getWinner(board)).toBe(expectedWinner);
+    },
+  );
+  const diagonalWin1 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0],
+    [0, 0, 1, 2, 1, 0, 0],
+    [0, 1, 2, 1, 2, 0, 0],
+    [0, 2, 1, 2, 1, 2, 0],
+  ];
+  const diagonalWin2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0],
+    [0, 0, 2, 1, 1, 0, 0],
+    [0, 2, 1, 1, 2, 0, 0],
+    [0, 1, 2, 1, 2, 1, 0],
+  ];
+  it.each([
+    ["1", diagonalWin1, 1],
+    ["2", diagonalWin2, 2],
+  ])(
+    "should return %s if they have a diagonal four",
+    (_msg, board, expectedWinner) => {
+      expect(getWinner(board)).toBe(expectedWinner);
+    },
+  );
+  const diagonal2Win1 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 1, 2, 1, 0, 0],
+    [0, 0, 2, 1, 2, 1, 0],
+    [0, 2, 1, 2, 1, 2, 0],
+  ];
+  const diagonal2Win2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 0, 0, 0, 0],
+    [0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 2, 1, 2, 0, 0],
+    [0, 0, 1, 2, 1, 2, 0],
+    [0, 1, 2, 1, 2, 1, 0],
+  ];
+  it.each([
+    ["1", diagonal2Win1, 1],
+    ["2", diagonal2Win2, 2],
+  ])(
+    "should return %s if they have a diagonal four",
+    (_msg, board, expectedWinner) => {
+      expect(getWinner(board)).toBe(expectedWinner);
+    },
+  );
+});
+
+describe("isFull", () => {
+  it("should return false for an empty board", () => {
+    expect(
+      isFull([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+      ]),
+    ).toBe(false);
+  });
+  it("should return false for an ongoing board", () => {
+    expect(
+      isFull([
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0],
+        [0, 2, 1, 0, 2, 0, 0],
+        [0, 2, 1, 1, 2, 0, 2],
+        [0, 1, 1, 2, 1, 1, 1],
+        [0, 2, 2, 1, 1, 2, 2],
+      ]),
+    ).toBe(false);
+  });
+  it("should return true for a full board", () => {
+    expect(
+      isFull([
+        [1, 1, 2, 2, 1, 1, 2],
+        [2, 2, 1, 1, 2, 2, 1],
+        [1, 1, 2, 2, 1, 1, 2],
+        [2, 2, 1, 1, 2, 2, 1],
+        [1, 1, 2, 2, 1, 1, 2],
+        [2, 2, 1, 1, 2, 2, 1],
+      ]),
+    ).toBe(true);
   });
 });
