7766954a625450caaaa19fc5652bde3da5c0d953
Julien MAIRE <Julien@privatejmembp.lan>
2025-01-20 16:02:28 +0100

    some cleaning. Added conf files (eslint, tsconfig ...).

--- apps/spa/src/index.tsx
+++ apps/spa/src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App";
+import App from "./app";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
